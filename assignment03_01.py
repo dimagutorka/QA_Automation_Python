ddcd08bff4bc6d3db36b345ae8f23d29b95f4035
dimagutorka <dgutorka@gmail.com>
2022-07-21 23:33:18 +0300

    Second commit, fixed HW 1

--- assignment03_01.py
+++ assignment03_01.py
@@ -7,11 +7,11 @@ Cy = int(input("Введіть Су: "))
 r1 = int(input("Введіть r1: "))
 r2 = int(input("Введіть r2: "))
 
-L = sqrt((Px - Cx) ^ 2 + (Py - Cy) ^ 2)
-print(L)
+L = sqrt((Px - Cx) ** 2 + (Py - Cy) ** 2)
 
 result = "не належить"
 if r1 < L < r2:
     result = "належить"
 
 print(f"Координата ({Px}, {Py}) {result} кільцю за центром у ({Cx}, {Cy}) та радіусами r1 = {r1}, r2 = {r2}")
+print(float(L))
